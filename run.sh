1bb2eae4c0cc5512b871938d985dd9f41d9eac03
wwwouter <wouter@wercker.com>
2013-06-21 16:01:22 +0200

    First commit.

---
+++ run.sh
@@ -0,0 +1,11 @@
+sudo apt-get install rsync
+
+export exclude_parameters=""
+
+for x in $WERCKER_COPY_OUTPUT_EXCLUDE
+do
+    export exclude_parameters="$exclude_parameters --exclude=$x "
+done
+
+echo "rsync -rv $exclude_parameters $WERCKER_ROOT/ $rsync"
+rsync -rv $exclude_parameters $WERCKER_ROOT/ $WERCKER_OUTPUT_DIR
